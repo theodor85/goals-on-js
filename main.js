882d38737e33dd3ce9b9d99ae6373aafe6491a6d
Fedor Kosolapov <fedor_coder@mail.ru>
2020-03-04 17:29:35 +0700

    use indexedDB

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 "use strict";
 
+initDB();
+
 // при старте показываем Цели
 turnOnGoals();
 turnOffActions();
+
+// получаем цели из БД
+let goals = getGoals();
+console.log(goals);
+
+// отображем цели
+for (let i = 0; i < goals.length; i++) {
+    const goal = goals[i];
+    displayGoalDiv(goal.name, goal.target, goal.current);
+}
 
 
 // обработчик выбора меню Цели
@@ -48,6 +60,17 @@ function onAddGoal(){
         return;
     }
 
+    displayGoalDiv(divGoalName.value, goalNumericArgument.value, 0);
+
+    // сохранение в БД
+    addNewGoal(divGoalName.value, goalNumericArgument.value);
+
+    // очищаем поля ввода
+    divGoalName.value = "";
+    goalNumericArgument.value = "";
+}
+
+function displayGoalDiv(goalName, goalTarget, goalCurrent) {
     // отображаем div с новой целью
     /* добавляем вот такой div:
     <div>
@@ -58,20 +81,9 @@ function onAddGoal(){
         </div>
     </div>
     */
-    let divGoals = document.getElementById("goals");    
-    let div = document.createElement('div');
-    div.innerHTML = '<div><p style="margin-top: 20px; margin-bottom: 0;">' + divGoalName.value + ' - 0% (0/' + goalNumericArgument.value + ')</p><div class="progress" style="margin-top: 0;"><div class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div></div></div>';
+   let divGoals = document.getElementById("goals");    
+   let div = document.createElement('div');
+   div.innerHTML = '<div><p style="margin-top: 20px; margin-bottom: 0;">' + goalName + ' - 0% (0/' + goalTarget + ')</p><div class="progress" style="margin-top: 0;"><div class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div></div></div>';
 
-    divGoals.appendChild(div);
-
-    // сохранение в БД
-    addNewGoal(divGoalName.value, goalNumericArgument.value);
-
-    // очищаем поля ввода
-    divGoalName.value = "";
-    goalNumericArgument.value = "";
-
-
-    
+   divGoals.appendChild(div);
 }
-
